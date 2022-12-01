793d1105b0d86dc178aa80ce9621ea85c24ce42b
m4mustafa110 <m4mustafa110@gmail.com>
2022-12-02 00:43:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const cooki = document.cookie
+console.log(cooki)
+document.getElementById("conctions").innerHTML = "hello"
+document.getElementById("messages").innerHTML = "hello"
+document.getElementById("following").innerHTML = "hello"

6424ae5319d51d089b6f4ca63ecca171431cb0c8
m4mustafa110 <m4mustafa110@gmail.com>
2022-12-02 18:09:58 +0530

    Fetched data from voyager api

---
+++ popup.js
@@ -0,0 +1,62 @@
+const profileNetworkApi = "https://www.linkedin.com/voyager/api/identity/profiles/ACoAAAC2eqYB5Eg-JCzGJxAmnBCGS72UBnYyaA8/networkinfo";
+const converstionsApi = "https://www.linkedin.com/voyager/api/messaging/conversations";
+
+const domain = "https://www.linkedin.com";
+
+
+async function setConnectionCount(domain, api) {
+    const JSESSIONID = await chrome.cookies.get({"url": domain, "name": "JSESSIONID"});
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', api, true);
+    xhr.setRequestHeader("csrf-token", JSESSIONID.value.replace(/"/g, ''));
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            let data = JSON.parse(this.response);
+            document.getElementById("connectionsCount").innerHTML = data.connectionsCount;
+        }
+    }
+
+    xhr.send();
+}
+async function setMessagesCount(domain, api) {
+    const JSESSIONID = await chrome.cookies.get({"url": domain, "name": "JSESSIONID"});
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', api, true);
+    xhr.setRequestHeader("csrf-token", JSESSIONID.value.replace(/"/g, ''));
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            let data = JSON.parse(this.response);
+            let messageCount = 0;
+            data.elements.forEach(element => {
+                messageCount = messageCount + element.totalEventCount;
+            });
+            document.getElementById("messagesCount").innerHTML = messageCount;
+        }
+    }
+
+    xhr.send();
+}
+async function setFollowingCount(domain, api) {
+    const JSESSIONID = await chrome.cookies.get({"url": domain, "name": "JSESSIONID"});
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', api, true);
+    xhr.setRequestHeader("csrf-token", JSESSIONID.value.replace(/"/g, ''));
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            let data = JSON.parse(this.response);
+            document.getElementById("followersCount").innerHTML = data.followersCount;
+        }
+    }
+
+    xhr.send();
+}
+
+setConnectionCount(domain, profileNetworkApi);
+setMessagesCount(domain, converstionsApi);
+setFollowingCount(domain, profileNetworkApi);
--- script.js
+++
@@ -1,5 +0,0 @@
-const cooki = document.cookie
-console.log(cooki)
-document.getElementById("conctions").innerHTML = "hello"
-document.getElementById("messages").innerHTML = "hello"
-document.getElementById("following").innerHTML = "hello"
